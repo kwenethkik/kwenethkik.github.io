d32c44fe2d9277ab2bbedfbc4f8a3907fd837306
kwenethkik <126173832+kwenethkik@users.noreply.github.com>
2025-09-24 17:59:08 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,12 @@
+// Exemple : petit effet pour la navigation
+document.addEventListener("DOMContentLoaded", () => {
+  const links = document.querySelectorAll("nav ul li a");
+  
+  links.forEach(link => {
+    link.addEventListener("click", (e) => {
+      e.preventDefault();
+      const section = document.querySelector(link.getAttribute("href"));
+      section.scrollIntoView({ behavior: "smooth" });
+    });
+  });
+});
